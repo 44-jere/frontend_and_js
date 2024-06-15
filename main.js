4a7dde840e6f0feb37f375c7fe2b46376b527c66
Jeremy Bryan Ico Herrera <jeremyico78@gmail.com>
2024-06-14 22:04:19 -0600

    integracion de menu mobile y modificacion de los media querys

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const menuMail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+function show(){
+    desktopMenu.classList.toggle("inactive")
+    mobileMenu.classList.toggle("inactive")
+}
 
 menuMail.addEventListener("click",e=>{
-    desktopMenu.classList.toggle("inactive")
+    show()
+})
+
+menuHamIcon.addEventListener("click",e=>{
+    show()
 })
