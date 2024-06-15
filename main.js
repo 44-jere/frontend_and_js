cabc5dda0c7afc4a678b5d50591ff5d52ea38a87
Jeremy Bryan Ico Herrera <jeremyico78@gmail.com>
2024-06-14 22:58:45 -0600

    agregar productos y modificar logica al mostrar el menu

--- main.js
+++ main.js
@@ -2,16 +2,28 @@ const menuMail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
-
-function show(){
-    desktopMenu.classList.toggle("inactive")
-    mobileMenu.classList.toggle("inactive")
+const aside = document.querySelector(".product-detail")
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const inactiveClass = "inactive"
+function hide(element){
+    element.classList.add(inactiveClass)
+}
+function showMenus(){
+    desktopMenu.classList.toggle(inactiveClass)
+    mobileMenu.classList.toggle(inactiveClass)
+    hide(aside)
 }
 
 menuMail.addEventListener("click",e=>{
-    show()
+    showMenus()
 })
 
 menuHamIcon.addEventListener("click",e=>{
-    show()
+    showMenus()
+})
+
+menuCarritoIcon.addEventListener("click",e=>{
+    aside.classList.toggle(inactiveClass)
+    hide(desktopMenu)
+    hide(mobileMenu)
 })
